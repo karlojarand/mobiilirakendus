da8b8d7275d23f4269f526bd2ce4e28dc3a05a0c
Karl Ojarand <karl@play.ee>
2020-11-19 12:01:26 +0200

    moblarakendus

--- App.js
+++ App.js
@@ -1,21 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, View, Text, } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet, TouchableOpacity, Image, TextInput } from 'react-native';
 
-export default function App() {
+
+//LIIKUMINE ERINEVATE LEHTEDE VAHEL HOME->DETAILS
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#DEDED9', marginBottom: -20 }}>
+      {/* <Text>Home Screen</Text> */}
+        <Image  style={{
+            resizeMode: "cover",
+            height: 250,
+            width: 120,
+            
+
+
+          }} source={require("./assets/basket.png")}/>  
+      
+        <Image
+          style={{
+            resizeMode: "cover",
+            height: 120,
+            width: 120
+          }}
+          source={require("./assets/huklab.png")} />
+
+      <View style={styles.screenContainer}>
+      <Button
+        title="PRESS TO START"
+        onPress={() => navigation.navigate('Details')}
+      />
+      </View>
     </View>
   );
 }
 
+
+
+//UUS LEHEKÜLG LISATUD
+
+
+
+
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#DEDED9' }}>
+      <Text style={styles.titleText}>Add Player:</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Disc Golf Putting League" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+<Stack.Screen name="Home">
+  {props => <HomeScreen {...props} extraData={someData} />}
+</Stack.Screen>
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screenContainer: {
+    fontSize: 0,
+    paddingTop: 20,
+    fontWeight: "bold",
   },
+  titleText: {
+    fontWeight: "bold",
+    fontSize: 30,
+  }
 });
+
+export default App;

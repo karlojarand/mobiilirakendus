e54dde4362f5dfd91d7c91a8c4f6d42eafb074df
Karl Ojarand <karl.ojarand@khk.ee>
2020-11-26 18:53:43 +0200

    button ja input

--- App.js
+++ App.js
@@ -2,7 +2,10 @@ import * as React from 'react';
 import { Button, View, Text, } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet, TouchableOpacity, Image, TextInput } from 'react-native';
+import { StyleSheet, TouchableOpacity, Image, TextInput, ScrollView } from 'react-native';
+import { ImageButton } from 'react-native-image-button-text';
+
+
 
 
 //LIIKUMINE ERINEVATE LEHTEDE VAHEL HOME->DETAILS
@@ -14,43 +17,47 @@ function HomeScreen({ navigation }) {
             resizeMode: "cover",
             height: 250,
             width: 120,
-            
-            
+          
 
+          }} source={require("./assets/basket.png")}/> 
 
-          }} source={require("./assets/basket.png")}/>  
-      
-        <Image
-          style={{
-            resizeMode: "cover",
-            height: 120,
-            width: 120
-          }}
-          source={require("./assets/huklab.png")} />
-
-      <View style={styles.screenContainer}>
-      <Button
+{/* ESILEHE NUPU SEADISTUS */}
+<TouchableOpacity>
+  <ImageButton width={120} height={12} text=""/>
+  <ImageButton style ={{marginTop:0}}
+    width={120}
+    height={120}
+    text=""
+    onPress={() => navigation.navigate('Details')}
+    source={require('./assets/huklab.png')}/>
+  
+    <Button
         title="PRESS TO START"
-        onPress={() => navigation.navigate('Details')}
-      />
+        onPress={() => navigation.navigate('Details')
+      }/>
+</TouchableOpacity>   
       </View>
-    </View>
   );
 }
 
-
-
 //UUS LEHEKÜLG LISATUD
 
-
-
-
-
 function DetailsScreen() {
+
+  // const [name, setName] = useState('karl');
+  // const [age, setAge] = useState('30');
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#DEDED9' }}>
-      <Text style={styles.titleText}>Add Player:</Text>
-    </View>
+    //TEXT INPUT
+     <View style={styles.container}>
+       <Text style={styles.titleText}>Add Player:</Text>
+       <TextInput style={styles.input}
+       placeholder='Enter your name'
+       onChangeText={(val) =>setName(val)}
+       />
+       
+     </View>
+     
   );
 }
 
@@ -72,6 +79,8 @@ function App() {
 </Stack.Screen>
 
 
+//STYLE
+
 const styles = StyleSheet.create({
   screenContainer: {
     fontSize: 0,
@@ -81,6 +90,21 @@ const styles = StyleSheet.create({
   titleText: {
     fontWeight: "bold",
     fontSize: 30,
+    textAlign: 'center',
+  },
+  //INPUTI STIIL
+  input: {
+    textAlign: 'center',
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
+  },
+  ImageIconStyle: {
+    resizeMode: "cover",
+    height: 120,
+    width: 120
   }
 });
 
